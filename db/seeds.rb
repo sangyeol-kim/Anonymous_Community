6d3aca38aa397c305309c4c527e67974278e6c62
sangyeol kim <twysg@likelion.org>
2017-08-30 23:05:35 +0000

    헤로쿠..

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+test_user1 = User.create( email: 'test1@naver.com', nickname: '익명1',  password: '123456' )
+test_user2 = User.create( email: 'test2@naver.com', nickname: '익명2',  password: '123456' )
+test_user3 = User.create( email: 'test3@naver.com', nickname: '익명3',  password: '123456' )
+test_user4 = User.create( email: 'test4@naver.com', nickname: '익명4',  password: '123456' )
+test_user5 = User.create( email: 'test5@naver.com', nickname: '익명5',  password: '123456' )
+test_user6 = User.create( email: 'test6@naver.com', nickname: '익명6',  password: '123456' )
+test_user7 = User.create( email: 'test7@naver.com', nickname: '익명7',  password: '123456' )
+test_user8 = User.create( email: 'test8@naver.com', nickname: '익명8',  password: '123456' )
+test_user9 = User.create( email: 'test9@naver.com', nickname: '익명9',  password: '123456' )
+test_user10 = User.create( email: 'test10@naver.com', nickname: '익명10',  password: '123456' )
+test_user11 = User.create( email: 'test11@naver.com', nickname: '익명11',  password: '123456' )
